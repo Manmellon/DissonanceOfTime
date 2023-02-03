06c2e6a1d94b3da00202f225f3e06d93a714e70a
Manmellon <24492231+Manmellon@users.noreply.github.com>
2023-02-03 16:20:02 +0300

    Added Player controller

---
+++ Assets/Game/Scripts/Player.cs
@@ -0,0 +1,149 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Components")]
+    public CharacterController controller;
+    public Camera player_camera;
+
+    public AudioSource _audioSource;
+    public AudioClip _walkAudio;
+    public AudioClip _runAudio;
+
+    [Header("Settings")]
+    public float mouseSensitivity;
+
+    public float walkSpeed = 4.0f;
+    public float runMultiplier = 2.0f;
+    public float crouchMultiplier = 0.5f;
+
+    private Vector3 fallingVelocity;
+    public float gravity = -9.8f;
+    public float jumpHeight;
+
+    private float xRotation;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public bool isGrounded;
+    public bool isCrouch;
+
+    public static Player singleton;
+
+
+    void Awake()
+    {
+        if (singleton == null) singleton = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && fallingVelocity.y < 0)
+        {
+            fallingVelocity.y = -2.0f;
+        }
+
+        if (isGrounded && Input.GetKey(KeyCode.Space))
+        {
+            isGrounded = false;
+
+            fallingVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+            //jumpTimer = 1;
+            //anim.SetBool("Jumping", true);
+        }
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * horizontalInput + transform.forward * verticalInput;
+
+        float resultSpeed = walkSpeed;
+        if (horizontalInput != 0 || verticalInput != 0)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                resultSpeed *= runMultiplier;
+                if (isGrounded)
+                {
+                    _audioSource.clip = _runAudio;
+                }
+                else
+                {
+                    _audioSource.clip = null;
+                }
+
+            }
+            else if (isCrouch)
+            {
+                resultSpeed *= crouchMultiplier;
+                if (isGrounded)
+                {
+                    _audioSource.clip = null;
+                }
+                else
+                {
+                    _audioSource.clip = null;
+                }
+            }
+            else
+            {
+                _audioSource.clip = _walkAudio;
+            }
+
+            if (!_audioSource.isPlaying)
+                _audioSource.Play();
+        }
+        else
+        {
+            _audioSource.clip = null;
+        }
+
+        if (Input.GetKeyDown(KeyCode.C) || Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            isCrouch = !isCrouch;
+
+            if (isCrouch)
+            {
+                controller.height -= 1f;
+                //groundCheck.localPosition -= new Vector3(0, 0.25f, 0);
+                //transform.position -= new Vector3(0, 0.25f, 0);
+
+            }
+            else
+            {
+                controller.height += 1f;
+                //groundCheck.localPosition += new Vector3(0, 0.25f, 0);
+                //transform.position += new Vector3(0, 0.25f, 0);
+            }
+        }
+
+        controller.Move(move * resultSpeed * Time.deltaTime);
+
+        fallingVelocity.y += gravity * Time.deltaTime;
+        controller.Move(fallingVelocity * Time.deltaTime);
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;// * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;// * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
+        //transform.localRotation = Quaternion.EulerAngles(xRotation, 0, 0);
+        player_camera.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
