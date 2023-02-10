6ef74d2f4f8289335ebbc6af3e82f42392f05710
Manmellon <24492231+Manmellon@users.noreply.github.com>
2023-02-10 21:26:07 +0300

    no message

--- Assets/Game/Scripts/ChronoWall.cs
+++ Assets/Game/Scripts/ChronoWall.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class ChronoWall : MonoBehaviour
 {
+    public MeshRenderer meshRenderer;
+    public MeshFilter meshFilter;
+
+    public ChronoPillar chronoPillarA;
+    public ChronoPillar chronoPillarB;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,9 @@ public class ChronoWall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        meshFilter.mesh.vertices[0] = chronoPillarA.topVertex.position;
+        meshFilter.mesh.vertices[1] = chronoPillarA.bottomVertex.position;
+        meshFilter.mesh.vertices[2] = chronoPillarB.bottomVertex.position;
+        meshFilter.mesh.vertices[3] = chronoPillarB.topVertex.position;
     }
 }
