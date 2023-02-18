3c34c53e01ac13dd6cfc26400310c509ace5e726
Manmellon <24492231+Manmellon@users.noreply.github.com>
2023-02-18 21:28:27 +0300

    Added moving platforms, but they are bad...

--- Assets/Game/Scripts/Platform.cs
+++ Assets/Game/Scripts/Platform.cs
@@ -5,25 +5,35 @@ using UnityEngine;
 public class Platform : SwitchingEntity
 {
     [Header("Platform")]
-    public Vector3 offPosition;
-    public Vector3 onPosition;
+    public Transform offPosition;
+    public Transform onPosition;
 
     public float speed;
 
     public float startMovingTime;
     public Vector3 curDirection;
 
+    public Vector3 velocity;
+
     protected override void Update()
     {
         base.Update();
 
+        _rigidbody.MovePosition(_rigidbody.position + velocity * Time.deltaTime);
+
         if (isOn)
         {
-
+            if ((_rigidbody.position - offPosition.position).magnitude >= (onPosition.position - offPosition.position).magnitude)
+            {
+                velocity = _rigidbody.velocity = Vector3.zero;
+            }
         }
         else
         {
-
+            if ((_rigidbody.position - onPosition.position).magnitude >= (offPosition.position - onPosition.position).magnitude)
+            {
+                velocity = _rigidbody.velocity = Vector3.zero;
+            }
         }
     }
 
@@ -31,13 +41,15 @@ public class Platform : SwitchingEntity
     {
         base.TurnedOnAction();
 
-        curDirection = onPosition - offPosition;
+        curDirection = onPosition.position - offPosition.position;
+        velocity = _rigidbody.velocity = speed * curDirection.normalized;
     }
 
     protected override void TurnedOffAction()
     {
         base.TurnedOffAction();
 
-        curDirection = offPosition - onPosition;
+        curDirection = offPosition.position - onPosition.position;
+        velocity = _rigidbody.velocity = speed * curDirection.normalized;
     }
 }
